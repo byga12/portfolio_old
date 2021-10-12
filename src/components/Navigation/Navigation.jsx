db55386f944218d11686ebdd0d8caa47a5cb18ce
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-10-12 16:45:43 -0300

    Changed favicon, improved Navigation bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 import s from './Navigation.module.sass'
+
+
+
 export default function Navigation() {
-
-
+  const [location] = useLocation();
  
   return (
     <nav className={s.nav} >
       <ul className={s.links}>
-        <li><Link className={s.link} href="/">About me</Link></li>
-        <li><Link className={s.link} href="/projects">Projects</Link></li>
-        <li><Link className={s.link} href="/contact">Contact</Link></li>   
+        <li><Link className={`${s.link} ${location==='/'?s.active:''}`} href="/">About me</Link></li>
+        <li><Link className={`${s.link} ${location==='/projects'?s.active:''}`} href="/projects">Projects</Link></li>
+        <li><Link className={`${s.link} ${location==='/contact'?s.active:''}`} href="/contact">Contact</Link></li>   
       </ul>
     </nav>
   )
